ba1b2a33c64d1b9da7926f66d72cdee1d52ce597
JianiLi98 <jiani.li9988olv@gmail.com>
2025-09-28 17:58:32 +1000

    basic layout and logic of level3 with tutorial labels

--- scripts/Main3.gd
+++ scripts/Main3.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @export var death_y := 1200.0        
-@export var next_scene: String = "res://scenes/Main3.tscn"
+@export var next_scene: String = "res://scenes/start.tscn"
 
 var player: Node2D
 @onready var fade_rect: ColorRect = $CanvasLayer/FadeRect
@@ -18,13 +18,12 @@ func _ready() -> void:
 	
 	# 渐亮
 	var tw := create_tween()
-	tw.tween_property(fade_rect, "color:a", 0.0, 1.0)
+	tw.tween_property(fade_rect, "color:a", 0.0, 2.0)
 	
 	# label
 	tutorial1.visible = false
 	tutorial2.visible = false
 	_play_tutorial()
-	
 
 func _process(delta: float) -> void:
 	if is_fading:
@@ -38,12 +37,12 @@ func _process(delta: float) -> void:
 	if player.global_position.y > death_y:
 		_restart()
 	
-	# 撞到 Portal 进入下一关 level3
+	# 撞到 Portal 进入下一关
 	if $Portal and $Portal.has_overlapping_bodies():
 		if $Portal.get_overlapping_bodies().has(player):
 			SoundManager.play_sfx("win")
 			$Portal/AnimatedSprite2D.stop()
-			get_tree().change_scene_to_file("res://scenes/Main3.tscn")
+			get_tree().change_scene_to_file("res://scenes/start.tscn")
 
 
 func _restart() -> void:
@@ -73,15 +72,14 @@ func _fade_and_call(action: Callable) -> void:
 	# Step 2: 切场景
 	action.call()
 
-
 func _play_tutorial() -> void:
 	# ---------- 第一句 ----------
 	tutorial1.visible = true
 	tutorial1.modulate.a = 0.0
 
 	var tw1 = create_tween()
-	tw1.tween_property(tutorial1, "modulate:a", 1.0, 1.0) 
-	tw1.tween_interval(2.0)
+	tw1.tween_property(tutorial1, "modulate:a", 1.0, 1.0)
+	tw1.tween_interval(3.0)
 	tw1.tween_property(tutorial1, "modulate:a", 0.0, 1.0)
 	await tw1.finished
 
@@ -92,8 +90,8 @@ func _play_tutorial() -> void:
 	tutorial2.modulate.a = 0.0
 
 	var tw2 = create_tween()
-	tw2.tween_property(tutorial2, "modulate:a", 1.0, 1.0) 
-	tw2.tween_interval(2.0)
+	tw2.tween_property(tutorial2, "modulate:a", 1.0, 1.0)
+	tw2.tween_interval(3.0)
 	tw2.tween_property(tutorial2, "modulate:a", 0.0, 1.0)
 	await tw2.finished
 
